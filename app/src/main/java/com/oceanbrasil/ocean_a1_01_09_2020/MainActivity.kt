1849521351fd3aa6b38d514e3c7916598d03f5dc
Paulo Salvatore <paulo@salvatore.tech>
2020-09-01 20:18:07 -0300

    IDs atualizados e exemplo 1 adicionado (manipulação de textView)

--- app/src/main/java/com/oceanbrasil/ocean_a1_01_09_2020/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_a1_01_09_2020/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.oceanbrasil.ocean_a1_01_09_2020
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Exemplo 1
+        tvNome.text = "Paulo Salvatore"
     }
 }
