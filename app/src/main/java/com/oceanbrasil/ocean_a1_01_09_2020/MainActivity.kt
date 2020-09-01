da0f31d23ba1fb9eaaf9e69b199e57dfacf74d20
Paulo Salvatore <paulo@salvatore.tech>
2020-09-01 20:49:35 -0300

    Manipulação de elementos, validação de conteúdo, limpando formulários e pegando strings resources no código da Activity

--- app/src/main/java/com/oceanbrasil/ocean_a1_01_09_2020/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_a1_01_09_2020/MainActivity.kt
@@ -10,6 +10,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Exemplo 1
-        tvNome.text = "Paulo Salvatore"
+//        tvNome.text = "Paulo Salvatore"
+
+        // Exemplo 2
+//        btAtualizar.setOnClickListener {
+//            tvNome.text = "Paulo Salvatore"
+//        }
+
+        // Exemplo 3
+//        btAtualizar.setOnClickListener {
+//            tvNome.text = etNome.text
+//        }
+
+        // Exemplo 4
+//        btAtualizar.setOnClickListener {
+//            if (etNome.text.isBlank()) {
+//                etNome.error = "Digite um nome válido"
+//            } else {
+//                tvNome.text = etNome.text
+//            }
+//        }
+
+        // Exemplo 5
+//        btLimpar.setOnClickListener {
+//            etNome.text.clear()
+//            tvNome.text = "Paulo Salvatore"
+//        }
+
+        // Exemplo 6
+        btLimpar.setOnClickListener {
+            etNome.text.clear()
+            tvNome.text = getString(R.string.hint_nome)
+        }
+
+        // Exemplo 7
+        btAtualizar.setOnClickListener {
+            if (etNome.text.isBlank()) {
+                etNome.error = getString(R.string.error_nome)
+            } else {
+                tvNome.text = etNome.text
+            }
+        }
     }
 }
