dcba494304f4fdb7aec68d8190b6edd18916960f
Paulo Salvatore <paulo@salvatore.tech>
2020-09-01 21:01:42 -0300

    Desafio

--- app/src/main/java/com/oceanbrasil/ocean_a1_01_09_2020/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_a1_01_09_2020/MainActivity.kt
@@ -51,5 +51,21 @@ class MainActivity : AppCompatActivity() {
                 tvNome.text = etNome.text
             }
         }
+
+        /*
+        Desafio
+        1. Reconstruir o projeto apresentado em aula (sem copiar e colar o código, mas pode usar de referência)
+        2. Criar uma Activity
+        3. Adicionar um elemento de visualização de texto para exibição do resultado
+        4. Adicionar dois elementos de edição de texto, um para o nome e outro para a idade
+        5. Adicionar dois botões, um para enviar e outro para limpar
+        6. Quando clicar no botão enviar, fazer uma validação se todos os campos foram digitados
+        7. Se a validação passar, atualizar o elemento de visualização de texto para: "Olá NOME, você tem IDADE anos."
+        8. Caso não passe, atualizar o elemento de edição de texto problemático com uma mensagem de erro
+        9. Ao clicar no botão limpar, limpa todos os itens do formulário
+        10. Adicione todos os recursos de texto ao arquivo de strings.xml
+        11. Traduz o app para inglês e português
+        12. E todos os recursos de cores ao arquivo de colors.xml
+        */
     }
 }
